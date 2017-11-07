fd515868239906a1a52c2ee99fe7c2e6f004e22f
chenkaiC4 <chenkaic4@gmail.com>
2017-11-07 18:31:04 +0800

    auto get image type from PixelDataInfo

--- dicomutil/dicomutil.go
+++ dicomutil/dicomutil.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 
@@ -35,11 +37,36 @@ func main() {
 		for _, elem := range data.Elements {
 			if elem.Tag == dicomtag.PixelData {
 				data := elem.Value[0].(dicom.PixelDataInfo).Frames[0]
-				path := fmt.Sprintf("image.%d.jpg", n) // TODO: figure out the image format
+				imgType := getFormat(bytes.NewBuffer(data))
+				if imgType == "" {
+					imgType = "jpg" // set default format
+				}
+				path := fmt.Sprintf("image.%d.%s", n, imgType)
 				n++
 				ioutil.WriteFile(path, data, 0644)
 				fmt.Printf("%s: %d bytes\n", path, len(data))
 			}
 		}
 	}
+}
+
+func getFormat(file io.Reader) string {
+	bytes := make([]byte, 4)
+	n, _ := file.Read(bytes)
+	if n < 4 {
+		return ""
+	}
+	if bytes[0] == 0x89 && bytes[1] == 0x50 && bytes[2] == 0x4E && bytes[3] == 0x47 {
+		return "png"
+	}
+	if bytes[0] == 0xFF && bytes[1] == 0xD8 {
+		return "jpg"
+	}
+	if bytes[0] == 0x47 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x38 {
+		return "gif"
+	}
+	if bytes[0] == 0x42 && bytes[1] == 0x4D {
+		return "bmp"
+	}
+	return ""
 }
